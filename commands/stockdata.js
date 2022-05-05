fd0943d6852048d873c32af27c92d4b9146c6ee8
Jason Wang <jasonbln07@gmail.com>
2022-05-04 20:00:33 -0400

    comment touchups

--- commands/stockdata.js
+++ commands/stockdata.js
@@ -1,6 +1,7 @@
 const { MessageEmbed } = require('discord.js');
 const axios = require('axios').default;
 const chartCmd = require('./chart');
+const puppeteer = require('puppeteer');
 require('dotenv').config();
 
 let target = 1;
@@ -45,6 +46,20 @@ class sdCmd {
             
             execute(message, stock) {
                 axios.request(stock).then(response => {
+                    
+                    async function scrapeLogo(url) {
+                        const browser = await puppeteer.launch();
+                        const page = await browser.newPage();
+                        await page.goto(url);
+                        
+                        const [logo] = await page.$x('/html/body/div[2]/div[4]/div[3]/header/div/div[2]/img'); 
+                        const src = await logo.getProperty('src');
+                        const srcLink = await src.jsonValue();
+                        
+                        await browser.close();
+                        return srcLink;
+                    }
+                    
                     if (target === 1) {
                         if (response.data.hasOwnProperty([ 'Error Message' ])) {
                             message.reply('Invalid ticker. Type !help for assistance.');
@@ -68,6 +83,7 @@ class sdCmd {
                                     }
                                 }
                                 
+                                scrapeLogo(`https://www.tradingview.com/symbols/${this.entity}/`).then((srcLink) => {
                                 const dataEmbed = new MessageEmbed()
                                 .setColor('#8fe166')
                                 .setTitle(`${this.entity}`)
@@ -76,56 +92,59 @@ class sdCmd {
                                     iconURL: 'https://s.yimg.com/cv/apiv2/myc/finance/Finance_icon_0919_250x252.png', 
                                     url: `https://finance.yahoo.com/quote/${this.entity}/` })
                                     .setDescription(`Last Refresh: ${last.slice(0, -3)}` + ` ${metaData[ '6. Time Zone' ]} Time`)
-                                    .setThumbnail('https://i.imgur.com/AfFp7pu.png') // image scrape if legal
+                                    .setThumbnail(`${srcLink}`) // discord does not support SVG files so it does not render
                                     .addFields(
                                         { name: `Most Recent Quote (${this.time} min)`, value: finalStr, inline: true },)                                  
                                         .setImage(image)
                                         .setTimestamp()
                                         .setFooter({ text: 'Graphed with quickchart.js', iconURL: 'https://avatars.githubusercontent.com/u/10342521?s=200&v=4' });
                                         
-                                        message.reply({ embeds: [dataEmbed] })});
-                                    }
-                                } else if (target === 2) {
-                                    const matches = response.data.bestMatches;
-                                    if (matches.length === 0) {
-                                        message.reply('Sorry! No Matches Found. Perhaps check your spelling?');
-                                    } else {
-                                        const bestMatch = matches[0];
-                                        let ticker = bestMatch[ '1. symbol' ];
-                                        
-                                        let finalStr = '';
-                                        for (let key in bestMatch) {
-                                            if (bestMatch.hasOwnProperty(key)) {
-                                                if (key != '9. matchScore' && key != '2. name' && key != '3. type' && key != '1. symbol') {
-                                                    finalStr += (`${key}` + ` ${bestMatch[key]}\n`);
-                                                }
-                                            }
-                                        }
-                                        
-                                        const searchEmbed = new MessageEmbed()
-                                        .setColor('#0099ff')
-                                        .setTitle(`${bestMatch[ '2. name' ]}`)
-                                        .setAuthor({ 
-                                            name: 'Visit Yahoo Finance', 
-                                            iconURL: 'https://s.yimg.com/cv/apiv2/myc/finance/Finance_icon_0919_250x252.png', 
-                                            url: `https://finance.yahoo.com/quote/${ticker}/` })
-                                            .setDescription(`Ticker: ${ticker}\nType: ${bestMatch[ '3. type' ]}`)
-                                            .setThumbnail('https://i.imgur.com/AfFp7pu.png') // image scrape if legal
-                                            .addFields(
-                                                { name: 'Other Information', value: finalStr, inline: true},
-                                                { name: '\u200B', value: '\u200B' },
-                                                { name: `Get a quote using the command below:`, 
-                                                value: `!stockdata ${ticker} <time interval>\nType !help for additional usage info`, inline: true},)
-                                                .setTimestamp()
-                                                .setFooter({ text: 'Data from Alpha Vantage API', iconURL: 'https://miro.medium.com/max/1024/1*UCZCB7Vx3EJ9FN-pen4BqQ.png' });
-                                                
-                                                message.reply({ embeds: [searchEmbed] });
-                                            }
-                                        }
-                                    }).catch(error => {
-                                        console.error(error);
+                                        message.reply({ embeds: [dataEmbed] })}); 
                                     });
                                 }
+                            } else if (target === 2) {
+                                const matches = response.data.bestMatches;
+                                if (matches.length === 0) {
+                                    message.reply('Sorry! No Matches Found. Perhaps check your spelling?');
+                                } else {
+                                    const bestMatch = matches[0];
+                                    let ticker = bestMatch[ '1. symbol' ];
+                                    
+                                    let finalStr = '';
+                                    for (let key in bestMatch) {
+                                        if (bestMatch.hasOwnProperty(key)) {
+                                            if (key != '9. matchScore' && key != '2. name' && key != '3. type' && key != '1. symbol') {
+                                                finalStr += (`${key}` + ` ${bestMatch[key]}\n`);
+                                            }
+                                        }
+                                    }
+                                    
+                                    scrapeLogo(`https://www.tradingview.com/symbols/${ticker}/`).then((srcLink) => {
+                                    const searchEmbed = new MessageEmbed()
+                                    .setColor('#0099ff')
+                                    .setTitle(`${bestMatch[ '2. name' ]}`)
+                                    .setAuthor({ 
+                                        name: 'Visit Yahoo Finance', 
+                                        iconURL: 'https://s.yimg.com/cv/apiv2/myc/finance/Finance_icon_0919_250x252.png', 
+                                        url: `https://finance.yahoo.com/quote/${ticker}/` })
+                                        .setDescription(`Ticker: ${ticker}\nType: ${bestMatch[ '3. type' ]}`)
+                                        .setThumbnail(`${srcLink}`) // discord does not support SVG files so it does not render
+                                        .addFields(
+                                            { name: 'Other Information', value: finalStr, inline: true},
+                                            { name: '\u200B', value: '\u200B' },
+                                            { name: `Get a quote using the command below:`, 
+                                            value: `!stockdata ${ticker} <time interval>\nType !help for additional usage info`, inline: true},)
+                                            .setTimestamp()
+                                            .setFooter({ text: 'Data from Alpha Vantage API', iconURL: 'https://miro.medium.com/max/1024/1*UCZCB7Vx3EJ9FN-pen4BqQ.png' });
+                                            
+                                            message.reply({ embeds: [searchEmbed] });
+                                        })
+                                    }
+                                }
+                            }).catch(error => {
+                                    console.error(error);
+                                });
                             }
-                            
-                            module.exports = sdCmd;
+                        }
+                                        
+module.exports = sdCmd;
